5cf1067c089215dfa414185cfd1002b4340538a0
Zizwar <zizwar@gmail.com>
2023-04-24 03:06:47 +0000

    fix redirect

--- routes/api/auth/google.ts
+++ routes/api/auth/google.ts
@@ -3,7 +3,7 @@ import { Handler } from "$fresh/server.ts";
 export const handler: Handler = (req: Request): Response => {
   const url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
   url.searchParams.set("client_id", Deno.env.get("GOOGLE_CLIENT_ID") || "");
-  url.searchParams.set("redirect_uri", Deno.env.get("PATH_RESTRICT") || "");
+  url.searchParams.set("redirect_uri", Deno.env.get("REDIRECT_URI") || "");
   url.searchParams.set(
     "scope",
     "https://www.googleapis.com/auth/userinfo.email"
