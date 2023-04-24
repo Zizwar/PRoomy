89f31e3b6ddbd1806070e975436a5529c0459acc
Zizwar <zizwar@gmail.com>
2023-04-24 03:55:00 +0000

    Fix BroadcastChannel error in RoomChannel class

--- communication/channel.ts
+++ communication/channel.ts
@@ -6,40 +6,50 @@ import type {
 } from "./types.ts";
 
 export class RoomChannel {
-  #channel: BroadcastChannel;
+  #channel: BroadcastChannel | undefined; // Declare #channel as possibly undefined
 
   constructor(roomId: number) {
-    this.#channel = new BroadcastChannel(roomId.toString());
+    if (typeof BroadcastChannel !== "undefined") { // Check if BroadcastChannel is defined
+      this.#channel = new BroadcastChannel(roomId.toString()); // Assign #channel if BroadcastChannel is defined
+    }
   }
 
   onMessage(handler: (message: ChannelMessage) => void) {
-    const listener = (e: MessageEvent) => {
-      handler(e.data);
+    if (!this.#channel) { // Check if #channel is defined
+      return { unsubscribe: () => {} }; // Return an empty object if #channel is undefined
+    }
+
+    const listener = (e: MessageEvent) => { // Define the listener function
+      handler(e.data); // Call the handler function with the message data
     };
-    this.#channel.addEventListener("message", listener);
+    this.#channel.addEventListener("message", listener); // Add the listener to the #channel
     return {
       unsubscribe: () => {
-        this.#channel.removeEventListener("message", listener);
+        this.#channel?.removeEventListener("message", listener); // Remove the listener from the #channel
       },
     };
   }
 
   close() {
-    this.#channel.close();
+    this.#channel?.close(); // Close the #channel if it exists
   }
 
   sendText(message: Omit<RoomTextChannelMessage, "kind">) {
-    this.#channel.postMessage({
-      kind: "text",
-      ...message,
-    });
+    if (this.#channel) { // Check if #channel is defined
+      this.#channel.postMessage({
+        kind: "text",
+        ...message,
+      });
+    }
   }
 
   sendIsTyping(user: UserView) {
-    const message: RoomIsTypingChannelMessage = {
-      kind: "isTyping",
-      from: user,
-    };
-    this.#channel.postMessage(message);
+    if (this.#channel) { // Check if #channel is defined
+      const message: RoomIsTypingChannelMessage = {
+        kind: "isTyping",
+        from: user,
+      };
+      this.#channel.postMessage(message);
+    }
   }
 }
--- routes/api/auth/google.ts
+++ routes/api/auth/google.ts
@@ -1,6 +1,6 @@
 import { Handler } from "$fresh/server.ts";
 
-export const handler: Handler = (req: Request): Response => {
+export const handler: Handler = (): Response => {
   const url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
   url.searchParams.set("client_id", Deno.env.get("GOOGLE_CLIENT_ID") || "");
   url.searchParams.set("redirect_uri", Deno.env.get("REDIRECT_URI") || "");
