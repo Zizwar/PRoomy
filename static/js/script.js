059653ce5a72a552f2d0d22976c0f3fe13046749
Zizwar <zizwar@gmail.com>
2023-04-26 07:13:29 +0000

    fixx style in script static

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.querySelector(".dark-light");
 const colors = document.querySelectorAll(".color");
 const toggleSettings = document.querySelector(".settings");
+const rooms = document.querySelector(".logo");
+
+rooms.addEventListener("click", ()=>handleColorClick(1));
+
 
 function handleColorClick() {
   colors.forEach((color) => color.classList.remove("selected"));
@@ -19,19 +23,25 @@ function toggleDarkMode() {
 
 toggleButton.addEventListener("click", toggleDarkMode);
 
-function handleSettingsClick() {
+function handleSettingsClick(rooms) {
   const chatArea = document.querySelector(".chat-area");
   const detailArea = document.querySelector(".detail-area");
   const conversationArea = document.querySelector(".conversation-area");
 
+  
   const displayDetailArea = window.getComputedStyle(detailArea).display;
   const displayConversationArea =
     window.getComputedStyle(conversationArea).display;
 
   if (displayDetailArea !== "flex") {
     chatArea.classList.toggle("displayNone");
-    detailArea.classList.toggle("displayBlock");
+    conversationArea.classList.toggle("displayBlock");
 
+    if(rooms){
+      conversationArea.classList.toggle("displayBlock");
+       chatArea.classList.toggle("displayNone");
+    detailArea.classList.toggle("displayNone");
+    }
     if (displayConversationArea !== "flex") {
       detailArea.classList.toggle("fullWidth");
     }
--- static/js/wino.js
+++
@@ -1,1 +0,0 @@
-console.log("wino.js pre")
