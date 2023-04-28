25197141193d37e9492ab800d444747edf2bee99
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-04-28 13:44:42 +0000

    stream open ai send.ts

--- routes/api/send.ts
+++ routes/api/send.ts
@@ -6,21 +6,32 @@ import { RoomChannel } from "@/communication/channel.ts";
 import { badWordsCleanerLoader } from "@/helpers/bad_words.ts";
 import { ApiSendMessage } from "@/communication/types.ts";
 
-import { OpenAI } from "openai";
+const openAI = new OpenAI(Deno.env.get("KEY_OPEN_AI") ?? "");
 
-export async function handler(
-  req: Request,
-  _ctx: HandlerContext
-): Promise<Response> {
+async function streamResults(openAI, query) {
+  const results = [];
+  while (true) {
+    const response = await openAI.createCompletion(query);
+    if (!response.choices) {
+      break;
+    }
+    results.push(response.choices[0].message.content);
+    query = results[results.length - 1];
+  }
+  return results;
+}
+
+async function handler(req, ctx) {
   const accessToken = getCookies(req.headers)["deploy_chat_token"];
   if (!accessToken) {
     return new Response("Not signed in", { status: 401 });
   }
+
   const database = await databaseLoader.getInstance();
- 
+
   const user = await database.getUserByAccessTokenOrThrow(accessToken);
   const data = (await req.json()) as ApiSendMessage;
-  const channel = new RoomChannel(data.roomId); 
+  const channel = new RoomChannel(data.roomId);
   const prooms = await database.getRoomPrompt(data.roomId);
 
   const from = {
@@ -50,43 +61,33 @@ export async function handler(
 
   ///paly ai
   if (message?.includes("@jpt")) {
-    const openAI = new OpenAI(Deno.env.get("KEY_OPEN_AI") ?? "");
+    const results = await streamResults(openAI, message.replace("@jpt", ""));
 
-    const from = {
-      name: "@JPT",
-      avatarUrl: "https://jpt.ma/favicon.ico",
-    };
-    channel.sendIsTyping(from);
+    // send the results to the channel
+    for (const result of results) {
+      channel.sendText({
+        message: result,
+        from,
+        createdAt: new Date().toISOString(),
+      });
+    }
 
-    const userContent = message.replace("@jpt", "");
-    const SystemRoleContenet = prooms 
-    console.log({prooms})
-
-    const chatCompletion = await openAI.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: SystemRoleContenet },
-        { role: "user", content: userContent },
-      ],
-    });
-
-    const choices = chatCompletion?.choices;
-
-    const text = choices[0]?.message.content;
-
+    // store the results
+    let text = "";
+    for (const result of results) {
+      text += result + " ";
+    }
     await database.insertMessage({
-      text: `${user.userId ?? ""}:${text}`,
+      text,
       roomId: data.roomId,
       userId: 12345666,
     });
-    channel.sendText({
-      message: `${user.userId ?? ""}:${text}`,
-      from,
-      createdAt: new Date().toISOString(),
-    });
   }
+
   //// end ai
   channel.close();
 
   return new Response("OK");
 }
+
+
