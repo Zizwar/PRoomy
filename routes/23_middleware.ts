9a99d25cd4397dc76a65aa9f44e6c67331953cef
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-04-30 21:06:07 +0100

    create 23_middleware.ts

--- routes/23_middleware.ts
+++ routes/23_middleware.ts
@@ -1,28 +1,34 @@
-
 import { getCookies } from "$std/http/cookie.ts";
-//import { supabase } from "@/lib/supabase.ts";
-/*import * as supabase from "supabase";
+import * as supabase from "supabase";
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 
-export const handler = async (req: Request, ctx: MiddlewareHandlerContext) => {
-	const cookies = getCookies(req.headers);
-	const access_token = cookies["supabase-access-token"];
+export const authenticateUser = async (req: Request, ctx: MiddlewareHandlerContext) => {
+  const authData = await getAuthData(req);
+  if (!authData.access_token) {
+    // The user is not authenticated.
+    return ctx.next();
+  }
 
-	if (access_token) {
-		const { data } = await supabase.auth.getUser(access_token);
+  const { data } = await supabase.auth.getUser(authData.access_token);
+  if (!data.user) {
+    // The user does not exist.
+    return ctx.next();
+  }
 
-		if (data.user) {
-			ctx.state.user = {
-				id: data.user.id,
-				email: data.user.email,
-				name: data.user.user_metadata?.full_name,
-				avatar_url: data.user.user_metadata?.avatar_url,
-				providers: data.user.app_metadata?.providers,
-				access_token,
-			};
-		}
-	}
+  ctx.state.user = {
+    id: data.user.id,
+    email: data.user.email,
+    name: data.user.user_metadata?.full_name,
+    avatar_url: data.user.user_metadata?.avatar_url,
+    providers: data.user.app_metadata?.providers,
+    access_token,
+  };
 
-	return ctx.next();
+  return ctx.next();
 };
-*/
+
+async function getAuthData(req: Request) {
+  const cookies = getCookies(req.headers);
+  const access_token = cookies["supabase-access-token"];
+  return { access_token };
+}
