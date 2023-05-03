a7e5b8e2a9234f567f4a375bc082b598a601d4fe
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-05-03 22:11:06 +0100

    Update chat-aria.tsx

--- islands/chat-aria.tsx
+++ islands/chat-aria.tsx
@@ -73,7 +73,7 @@ export default function Chat({
     server.sendMessage(roomId, input);
     setInput("");
   };
-  const TextAreaInput = () => {
+  const _TextAreaInput = () => {
     const [input, setInput] = useState("");
     const textareaRef = useRef(null);
   
@@ -172,7 +172,13 @@ export default function Chat({
         >
           <path d="M21.44 11.05l-9.19 9.19a6 6 0 01-8.49-8.49l9.19-9.19a4 4 0 015.66 5.66l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48" />
         </svg>
-        <TextAreaInput/>
+        <textarea type="text" value={input}
+          onInput={(e) => {
+            setInput(e.currentTarget.value);
+            server.sendIsTyping(roomId);
+          }}
+         class="text-area-input"
+           placeholder="Type something here..." />
         <svg onClick={(e) => send()} viewBox="0 0 513 513" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path d="M256.025.05C117.67-2.678 3.184 107.038.025 245.383a240.703 240.703 0 0085.333 182.613v73.387c0 5.891 4.776 10.667 10.667 10.667a10.67 10.67 0 005.653-1.621l59.456-37.141a264.142 264.142 0 0094.891 17.429c138.355 2.728 252.841-106.988 256-245.333C508.866 107.038 394.38-2.678 256.025.05z" class="feather feather-plus-circle" fill="#0086ff"></path>
           <path d="M330.518 131.099l-213.825 130.08c-7.387 4.494-5.74 15.711 2.656 17.97l72.009 19.374a9.88 9.88 0 007.703-1.094l32.882-20.003-10.113 37.136a9.88 9.88 0 001.083 7.704l38.561 63.826c4.488 7.427 15.726 5.936 18.003-2.425l65.764-241.49c2.337-8.582-7.092-15.72-14.723-11.078zM266.44 356.177l-24.415-40.411 15.544-57.074c2.336-8.581-7.093-15.719-14.723-11.078l-50.536 30.744-45.592-12.266L319.616 160.91 266.44 356.177z" fill="#fff"></path>
