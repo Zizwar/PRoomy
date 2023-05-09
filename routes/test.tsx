badd5bb7976bcc391ce9c97cc1fb5a122fa5c729
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-05-09 20:55:28 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,44 +1,131 @@
+import ChatArea from "@/islands/chat-aria.tsx";
+import Rooms from "@/islands/rooms.tsx";
+import Detail from "@/islands/detail.tsx";
+import Header from "@/islands/header.tsx";
+//
+import {  HandlerContext, PageProps, RouteConfig } from "$fresh/server.ts";
 
+import { getCookies, setCookie } from "$std/http/cookie.ts";
+import { databaseLoader } from "@/communication/database.ts";
+import { gitHubApi } from "@/communication/github.ts";
+import { googleApi } from "@/communication/google.ts";
 
-export default function NewRoom() {
-  return (
-    <>
+import type { RoomView } from "@/communication/types.ts";
+import type { MessageView, UserView } from "@/communication/types.ts";
+//
 
-<a
-onClick={()=>alert(23)}
-  href="#"
-  style={{backgroundImage:"url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)"}}
-  class="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat"
->
-  <div class="absolute inset-0 bg-black/25"></div>
-
-  <div class="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
-    <div class="sm:pt-18 pt-12 text-white lg:pt-24">
-      <h3 class="text-xl font-bold sm:text-2xl">Rome</h3>
-
-      <p onClick={()=>alert(23)} class="text-sm">Italy</p>
-    </div>
-
-    <span
-      class="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-    >
-      4.5
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4 text-yellow-300"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-        />
-      </svg>
-    </span>
-  </div>
-</a>
-
-
-    </> );
+//
+interface Data {
+  messages: MessageView[];
+  roomName: string;
+  prompt:string;
+  user: UserView;
+  rooms: RoomView[];
 }
 
+export default function Home({ url, data, params }: PageProps<Data>) {
+  return (
+    <>
+      <div class="app">
+        <Header user={data.user || []} />
+        {data && (
+          <div class="wrapper">
+            <Rooms url={url} data={data} />
+
+            <ChatArea
+              roomId={+params.room || 1}
+              initialMessages={data.messages}
+              roomName={data.roomName}
+              user={data.user}
+            />
+
+            <Detail roomId={+params.room || 1} name={data.roomName} prompt={data.prompt} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+export async function handler(
+  req: Request,
+  ctx: HandlerContext
+): Promise<Response> {
+  // Get cookie from request header and parse it
+  const maybeAccessToken = getCookies(req.headers)["deploy_chat_token"];
+  const database = await databaseLoader.getInstance();
+  if (maybeAccessToken) {
+    const user = await database.getUserByAccessToken(maybeAccessToken);
+    if (user) {
+  
+      const [rooms, messages, namePrompt] = await Promise.all([
+        database.getRooms(),
+        database.getRoomMessages(+ctx.params.room ?? 2),
+        database.getRoomNamePrompt(+ctx.params.room ?? 2),
+
+      ]);  
+     
+      const { name: roomName, prompt } = namePrompt
+      console.log()
+      const response = await ctx.render({
+        user,
+        rooms,
+        messages,
+        roomName,
+        prompt
+      });
+      return response;
+    }
+  }
+
+  // This is an oauth callback request.
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return ctx.render(false);
+  }
+
+  //  const accessToken = await gitHubApi.getAccessToken(code);
+  //const userData = await gitHubApi.getUserData(accessToken);
+  // Check the provider value from the query string
+  const scope = url.searchParams.get("scope");
+  let accessToken, userData;
+  if (scope?.includes("google")) {
+    accessToken = await googleApi.getAccessToken(code);
+    userData = await googleApi.getUserData(accessToken);
+  } else {
+    //console.log({ provider });
+    // Provider is github
+    accessToken = await gitHubApi.getAccessToken(code);
+    userData = await gitHubApi.getUserData(accessToken);
+  }
+  await database.insertUser({
+    userId: userData.userId,
+    userName: userData.userName,
+    accessToken,
+    avatarUrl: userData.avatarUrl,
+  });
+
+  const [rooms, messages, { name: roomName, prompt }] = await Promise.all([
+    database.getRooms(),
+    database.getRoomMessages(+ctx.params.room),
+    database.getRoomNamePrompt(+ctx.params.room),
+  ]);
+
+  const response = await ctx.render({
+    user: userData,
+    rooms,
+    messages,
+    roomName,
+    prompt
+  });
+  setCookie(response.headers, {
+    name: "deploy_chat_token",
+    value: accessToken,
+    maxAge: 60 * 60 * 24 * 7,
+    httpOnly: true,
+  });
+  return response;
+}
+export const config: RouteConfig = {
+  routeOverride: "/proomy/:room",
+};
