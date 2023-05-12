f621aab481bd10ff7da657bf96642b6c735af5fd
Zizwar <zizwar@gmail.com>
2023-05-12 06:06:06 +0000

    add img logo in header

--- islands/header.tsx
+++ islands/header.tsx
@@ -5,7 +5,7 @@ export default function Header({user}:{user:UserView}) {
     <>
       <div class="header">
         <div class="logo">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><path d="M12 8v8M8 12h8"></path></svg>
+        <img class="chat-msg-img" src="/favicon.ico" alt="JPT Logo"/>
 
         </div>
         <div class="search-bar">
