4f9b58f8ae03afcdfa696a8c89a1e58cd0003e74
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-05-22 09:44:14 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,6 +1,31 @@
 export default function Test() {
   return (
     <>
+
+<div id="modal-container">
+  <div class="modal-background">
+    <div class="modal">
+      <h2>I'm a Modal</h2>
+      <p>Hear me roar.</p>
+      <svg class="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none">
+								<rect x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"></rect>
+							</svg>
+    </div>
+  </div>
+</div>
+<div class="content">
+  <h1>Modal Animations</h1>
+  <div class="buttons">
+    <div id="one" class="button">Unfolding</div>
+    <div id="two" class="button">Revealing</div>
+    <div id="three" class="button">Uncovering</div>
+    <div id="four" class="button">Blow Up</div><br>
+    <div id="five" class="button">Meep Meep</div>
+    <div id="six" class="button">Sketch</div>
+    <div id="seven" class="button">Bond</div>
+  </div>
+</div>
+
 <div class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 h-screen flex overflow-hidden text-sm">
   <div class="bg-white dark:bg-gray-900 dark:border-gray-800 w-20 flex-shrink-0 border-r border-gray-200 flex-col hidden sm:flex">
     <div class="h-16 text-blue-500 flex items-center justify-center">
