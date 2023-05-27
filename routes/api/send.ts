62871b232e3b014f083cbb3186e8c7f32c60a6ac
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-05-27 21:37:31 +0100

    Update send.ts

--- routes/api/send.ts
+++ routes/api/send.ts
@@ -72,7 +72,7 @@ export async function handler(
     const text = choices[0]?.message.content ?? 'no result';
 
     await database.insertMessage({
-      text: `${user.userName || ""}:${text}`,
+      text: `${user.userName || ""}:\n ${text}`,
       roomId: data.roomId,
       userId: 12345666,
       to:user.userId,
