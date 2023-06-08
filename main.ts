98deb282862f357851c4b0d9acdfea3d3be5dacb
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-08 06:43:19 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,11 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-
 import twindPlugin from "$fresh/plugins/twind.ts";
+
+import manifest from "./fresh.gen.ts";
 import twindConfig from "./twind.config.ts";
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
