aa6686da31db06e91b2033b678f5396f100c88ad
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-11 18:08:13 +0100

    twind landing test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,13 +1,26 @@
 export default function Test() {
   return (
     <>
-   <div class="modal" id="my-modal">
-  <div class="modal-content">
-    <h2>This is my modal</h2>
-    <p>This is some text in my modal</p>
-    <button class="btn btn-primary" id="close-modal">Close</button>
-  </div>
-</div>
+   <div class="text-gray-700 bg-white">
+  <nav class="fixed bg-white w-full z-10 border-b border-solid border-gray-300">
+    <div class="container mx-auto px-6 py-2 flex justify-between items-center">
+      <a href="#" class="font-bold text-2xl lg:text-4xl">
+        TAILWIND
+      </a>
+      <div class="block md:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-gray-500 boder-gray-600 hiver:text-gray-800">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://wwww.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div class="hidden md:block">
+        <ul class="inline-flex">
+          <li>
+            <a href="#" class="px-4 font-bold hover:text-gray-800">
+              Home
+            </a>
 
     
     </>
