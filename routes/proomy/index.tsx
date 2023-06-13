69aaa85d9b6e71d8d448240dc9357491c8b74085
Zizwar <zizwar@gmail.com>
2023-06-13 01:58:22 +0000

    add sticker github

--- routes/proomy/index.tsx
+++ routes/proomy/index.tsx
@@ -140,6 +140,14 @@ export default function Main({ url, data }: PageProps<{ rooms: RoomView[] }>) {
           )}
         </div>
       </div>
+      <div
+        class="singleton-element"
+        style='position: absolute;top: 29px;left: -272px;width: 600px;height: 28px;margin: 0px;padding: 2px;font-size: 24px;text-align: center;color: rgb(255, 255, 255);font-family: "trebuchet ms", verdana, arial, sans-serif;transform: rotate(-45deg);transform-origin: 50% 0px;background-color: rgb(0, 0, 0);border: 1px solid rgb(170, 170, 170);opacity: 0.5;z-index: 1200;'
+      >
+        <a href="https://github.com/Zizwar/PRoomy/" alt="JPT Proomy">
+          Github
+        </a>
+      </div>
     </>
   );
 }
