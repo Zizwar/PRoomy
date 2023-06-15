8bd4c54ab1f65c673ad39a6041d5045ba61d2567
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-15 19:29:52 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -2,20 +2,34 @@ export default function Test() {
   return (
     <>
       
-    <div class="flex flex-col items-center justify-center min-h-screen py-2">
-        <div class="text-center">
-            <h1 class="text-4xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">Welcome to Roomy Prompt!</h1>
-            <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                Here, experience the power of AI in conversation. With Roomy Prompt, we offer ChatGPT powered chatrooms. Each room has its own bot, designed to answer questions and inquiries based on the room's header info.
-            </p>
-            <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mt-8">
-                This project is ideal for individuals and companies who wish to establish separate chat rooms for each project or their own platform. Whether you wish to provide live chat support for your project, or wish to encourage collaboration among members, Roomy Prompt is the solution for you.
-            </p>
-            <div class="mt-10">
-                <a href="https://github.com/Zizwar/PRoomy/" class="py-3 px-6 text-white bg-indigo-500 rounded-full font-bold leading-snug hover:bg-indigo-600 hover:text-white">Visit our Project on GitHub</a>
-            </div>
-        </div>
-    </div>
+    
+<div style="direction:rtl" class="bg-gray-100 text-gray-800">
+
+    <!-- Hero Section -->
+    <section class="hero bg-blue-600 text-white text-center py-20">
+        <h1 class="text-4xl font-bold">مرحبا بكم في Joint Public Technology - JPT.MA!</h1>
+        <p class="text-xl mt-4 px-24">نحن متخصصون في توظيف الذكاء الاصطناعي...</p>
+    </section>
+
+    <!-- About Section -->
+    <section class="about py-20 px-10">
+        <h2 class="text-3xl font-bold text-center">حولنا</h2>
+        <p class="text-xl mt-4 px-24">تعرف على رؤيتنا ومهمتنا وقيمنا.</p>
+    </section>
+
+    <!-- Services Section -->
+    <section class="services py-20 px-10">
+        <h2 class="text-3xl font-bold text-center">الخدمات</h2>
+        <p class="text-xl mt-4 px-24">تفاصيل حول مجموعة الخدمات التي نقدمها.</p>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact py-20 px-10">
+        <h2 class="text-3xl font-bold text-center">اتصل بنا</h2>
+        <p class="text-xl mt-4 px-24">نحن هنا للرد على أي أسئلة لديك وللمساعدة في تحقيق أهدافك الرقمية.</p>
+    </section>
+
+</div>
 
     </>
   );
