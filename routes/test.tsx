bbaf9fe0af0f84726ca5e597b3c29bd9cbf418da
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-15 19:09:28 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,14 +1,22 @@
 export default function Test() {
   return (
     <>
-      <div
-        class="singleton-element"
-        style='position: absolute;top: 29px;left: -272px;width: 600px;height: 28px;margin: 0px;padding: 2px;font-size: 24px;text-align: center;color: rgb(255, 255, 255);font-family: "trebuchet ms", verdana, arial, sans-serif;transform: rotate(-45deg);transform-origin: 50% 0px;background-color: rgb(0, 0, 0);border: 1px solid rgb(170, 170, 170);opacity: 0.5;z-index: 1200;'
-      >
-        <a href="https://github.com/Zizwar/PRoomy/" alt="JPT Proomy">
-          Github
-        </a>
-      </div>
+      
+    <div class="flex flex-col items-center justify-center min-h-screen py-2">
+        <div class="text-center">
+            <h1 class="text-4xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">Welcome to Roomy Prompt!</h1>
+            <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                Here, experience the power of AI in conversation. With Roomy Prompt, we offer ChatGPT powered chatrooms. Each room has its own bot, designed to answer questions and inquiries based on the room's header info.
+            </p>
+            <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mt-8">
+                This project is ideal for individuals and companies who wish to establish separate chat rooms for each project or their own platform. Whether you wish to provide live chat support for your project, or wish to encourage collaboration among members, Roomy Prompt is the solution for you.
+            </p>
+            <div class="mt-10">
+                <a href="https://github.com/Zizwar/PRoomy/" class="py-3 px-6 text-white bg-indigo-500 rounded-full font-bold leading-snug hover:bg-indigo-600 hover:text-white">Visit our Project on GitHub</a>
+            </div>
+        </div>
+    </div>
+
     </>
   );
 }
