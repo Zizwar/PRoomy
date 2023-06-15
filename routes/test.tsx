8a358d1c14d44a01521ab20772454c43f18e73b2
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-15 19:31:37 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -5,25 +5,25 @@ export default function Test() {
     
 <div style="direction:rtl" class="bg-gray-100 text-gray-800">
 
-    <!-- Hero Section -->
+   
     <section class="hero bg-blue-600 text-white text-center py-20">
         <h1 class="text-4xl font-bold">مرحبا بكم في Joint Public Technology - JPT.MA!</h1>
         <p class="text-xl mt-4 px-24">نحن متخصصون في توظيف الذكاء الاصطناعي...</p>
     </section>
 
-    <!-- About Section -->
+    
     <section class="about py-20 px-10">
         <h2 class="text-3xl font-bold text-center">حولنا</h2>
         <p class="text-xl mt-4 px-24">تعرف على رؤيتنا ومهمتنا وقيمنا.</p>
     </section>
 
-    <!-- Services Section -->
+    
     <section class="services py-20 px-10">
         <h2 class="text-3xl font-bold text-center">الخدمات</h2>
         <p class="text-xl mt-4 px-24">تفاصيل حول مجموعة الخدمات التي نقدمها.</p>
     </section>
 
-    <!-- Contact Section -->
+    
     <section class="contact py-20 px-10">
         <h2 class="text-3xl font-bold text-center">اتصل بنا</h2>
         <p class="text-xl mt-4 px-24">نحن هنا للرد على أي أسئلة لديك وللمساعدة في تحقيق أهدافك الرقمية.</p>
