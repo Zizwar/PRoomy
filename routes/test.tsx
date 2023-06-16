36e3b3b9bd30bb3c189ffacb3b95dfee8e1fdc30
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-16 20:44:02 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -1,36 +1,76 @@
 export default function Test() {
   return (
     <>
-      
-    
-<div style="direction:rtl" class="bg-gray-100 text-gray-800">
-
+     
+    <div class="bg-gray-100">
+      <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+        <header class="text-center">
+          <img src="/img/logo.png" alt="JPT.ma" />
+          <h1 class="text-4xl font-bold text-gray-800">Internet Marketing Made Easy</h1>
+        </header>
+        <section class="my-6">
+          <h2 class="text-2xl font-bold text-gray-800">Our Services</h2>
+          <ul class="list-disc mb-4">
+            <li class="text-lg leading-6 text-gray-600">
+              <strong class="text-gray-800">SEO</strong>: We can help you improve your
+              website's ranking in search engine results pages (SERPs). This can be
+              done through a variety of methods, such as keyword research, on-page
+              optimization, and off-page optimization.
+            </li>
+            <li class="text-lg leading-6 text-gray-600">
+              <strong class="text-gray-800">PPC</strong>: We can help you create and
+              manage PPC campaigns. PPC advertising allows you to place ads on search
+              engines and other websites. When a user clicks on an ad, you pay a fee
+              to the search engine or website.
+            </li>
+            <li class="text-lg leading-6 text-gray-600">
+              <strong class="text-gray-800">Social Media</strong>: We can help you
+              create and manage social media profiles, develop social media content,
+              and run social media campaigns. Social media marketing can be a great
+              way to reach a large audience and build relationships with potential
+              customers.
+            </li>
+            <li class="text-lg leading-6 text-gray-600">
+              <strong class="text-gray-800">Email</strong>: We can help you create and
+              send email newsletters and promotional emails. Email marketing is a
+              great way to stay in touch with customers and promote new products or
+              services.
+            </li>
+          </ul>
+        </section>
+        <section class="my-6">
+          <h2 class="text-2xl font-bold text-gray-800">Testimonials</h2>
+          <ul class="list-disc mb-4">
+            <li class="text-lg leading-6 text-gray-600">
+              "JPT.ma has been a great partner for our business. They have helped us
+              improve our website's ranking in search engine results pages, and they
+              have also helped us create and manage our social media accounts. We
+              are very happy with their services."
+              <br />
+              <strong class="text-gray-800">- John Smith, CEO of Acme Corporation</strong>
+            </li>
+            <li class="text-lg leading-6 text-gray-600">
+              "JPT.ma is a great company to work with. They are very knowledgeable
+              about Internet marketing, and they are always willing to go the extra
+              mile to help our business succeed."
+              <br />
+              <strong class="text-gray-800">- Jane Doe, Marketing Manager at XYZ Company</strong>
+            </li>
+            <li class="text-lg leading-6 text-gray-600">
+              "JPT.ma has helped us increase our website traffic and sales. We are
+              very happy with their services."
+              <br />
+              <strong class="text-gray-800">- Mary Johnson, Owner of ABC Store</strong>
+            </li>
+          </ul>
+        </section>
+        <footer class="text-center">
+          <a href="#top" class="text-gray-600">Back to top</a>
+        </footer>
+      </div>
+    </div>
+  
    
-    <section class="hero bg-blue-600 text-white text-center py-20">
-        <h1 class="text-4xl font-bold">مرحبا بكم في Joint Public Technology - JPT.MA!</h1>
-        <p class="text-xl mt-4 px-24">نحن متخصصون في توظيف الذكاء الاصطناعي...</p>
-    </section>
-
-    
-    <section class="about py-20 px-10">
-        <h2 class="text-3xl font-bold text-center">حولنا</h2>
-        <p class="text-xl mt-4 px-24">تعرف على رؤيتنا ومهمتنا وقيمنا.</p>
-    </section>
-
-    
-    <section class="services py-20 px-10">
-        <h2 class="text-3xl font-bold text-center">الخدمات</h2>
-        <p class="text-xl mt-4 px-24">تفاصيل حول مجموعة الخدمات التي نقدمها.</p>
-    </section>
-
-    
-    <section class="contact py-20 px-10">
-        <h2 class="text-3xl font-bold text-center">اتصل بنا</h2>
-        <p class="text-xl mt-4 px-24">نحن هنا للرد على أي أسئلة لديك وللمساعدة في تحقيق أهدافك الرقمية.</p>
-    </section>
-
-</div>
-
     </>
   );
 }
