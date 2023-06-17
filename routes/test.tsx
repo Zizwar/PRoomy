b8f85945a631e844e2194063b588efb41c022c7f
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-06-17 20:10:36 +0100

    Update test.tsx

--- routes/test.tsx
+++ routes/test.tsx
@@ -3,31 +3,31 @@ export default function Test() {
   return (
     <>
 <div class="container mx-auto max-w-screen-xl">
-    <header class="text-center mb-8">
-      <h1 class="text-3xl font-bold">Proomy Roomy Prompt</h1>
-      <p class="text-lg">A Moroccan-based website that provides a platform for users to create and share prompts.</p>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Moroccan_flag.svg/1200px-Moroccan_flag.svg.png" alt="Moroccan flag" class="mb-4">
-    </header>
-    <section class="mb-8">
-      <h2 class="text-2xl font-bold">What is a prompt?</h2>
-      <p class="text-lg">A prompt is a short, creative text prompt that can be used to generate different creative text formats, like poems, code, scripts, musical pieces, email, letters, etc.</p>
-      <p class="text-lg">Proomy Roomy Prompt is a great way to spark your creativity and come up with new ideas. Whether you're a writer, a musician, or a programmer, you'll find something to inspire you on Proomy Roomy Prompt.</p>
-    </section>
-    <section class="mb-8">
-      <h2 class="text-2xl font-bold">Features</h2>
-      <ul class="list-disc mb-4">
-        <li>Create and share prompts</li>
-        <li>Organize prompts into categories</li>
-        <li>Chat with other users</li>
-        <li>Export prompts to different formats</li>
-        <li>Access premium features with a subscription</li>
-      </ul>
-      <a href="/signup" class="btn btn-primary">Sign Up</a>
-    </section>
-    <footer class="text-center">
-      <p class="text-sm">Copyright © 2023 Proomy Roomy Prompt</p>
-    </footer>
-  </div>
+      <header class="text-center mb-8">
+        <h1 class="text-3xl font-bold">Proomy Roomy Prompt</h1>
+        <p class="text-lg">A Moroccan-based website that provides a platform for users to create and share prompts.</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Moroccan_flag.svg/1200px-Moroccan_flag.svg.png" alt="Moroccan flag" class="mb-4">
+      </header>
+      <section class="mb-8">
+        <h2 class="text-2xl font-bold">What is a prompt?</h2>
+        <p class="text-lg">A prompt is a short, creative text prompt that can be used to generate different creative text formats, like poems, code, scripts, musical pieces, email, letters, etc.</p>
+        <p class="text-lg">Proomy Roomy Prompt is a great way to spark your creativity and come up with new ideas. Whether you're a writer, a musician, or a programmer, you'll find something to inspire you on Proomy Roomy Prompt.</p>
+      </section>
+      <section class="mb-8">
+        <h2 class="text-2xl font-bold">Features</h2>
+        <ul class="list-disc mb-4">
+          <li>Create and share prompts</li>
+          <li>Organize prompts into categories</li>
+          <li>Chat with other users</li>
+          <li>Export prompts to different formats</li>
+          <li>Access premium features with a subscription</li>
+        </ul>
+        <a href="/signup" class="btn btn-primary">Sign Up</a>
+      </section>
+      <footer class="text-center">
+        <p class="text-sm">Copyright © 2023 Proomy Roomy Prompt</p>
+      </footer>
+    </div>
  </>
   );
 }
