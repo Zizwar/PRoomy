addc7a285fa0e41dcc7b409e5937e1829cdc4568
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-07-13 18:21:20 +0100

    Update index.tsx

--- routes/proomy/index.tsx
+++ routes/proomy/index.tsx
@@ -81,7 +81,19 @@ export default function Main({ url, data }: PageProps<{ rooms: RoomView[] }>) {
             <span class="block text-lg -mb-1.5">
               An intermediate open-source AI chat room platform template.
             </span>
-          </div>
+
+    <div class="flex flex-col items-center justify-center min-h-screen py-2"> 
+         <div class="text-center"> 
+            
+             <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"> 
+                 Here, experience the power of AI in conversation. With Roomy Prompt, we offer ChatGPT powered chatrooms. Each room has its own bot, designed to answer questions and inquiries based on the room's header info. 
+             </p> 
+             <p class="max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mt-8"> 
+                 This project is ideal for individuals and companies who wish to establish separate chat rooms for each project or their own platform. Whether you wish to provide live chat support for your project, or wish to encourage collaboration among members, Roomy Prompt is the solution for you. 
+             </p> 
+          
+         </div> 
+     </div>          </div>
           {data ? (
             <ul
               role="list"
